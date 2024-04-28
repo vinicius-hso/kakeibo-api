03c7066ab0b4dca8a1de7c9bdb4ea2186374d24f
vinicius-hso <viniciushso@icloud.com>
2024-04-28 12:23:32 -0300

    feat: add delete income controller

--- src/controllers/IncomeController.ts
+++ src/controllers/IncomeController.ts
@@ -15,29 +15,57 @@ export class IncomeController {
     if (!income.title)
       return response
         .status(400)
-        .json({ message: "Bad request: title required!" })
+        .json({ message: "Bad request: title required!" });
 
     if (!income.total)
       return response
         .status(400)
-        .json({ message: "Bad request: total required!" })
-        
+        .json({ message: "Bad request: total required!" });
+
     if (!income.date)
       return response
         .status(400)
-        .json({ message: "Bad request: date required!" })
+        .json({ message: "Bad request: date required!" });
 
     try {
-      const result = await this.incomeService.create(income)
+      const result = await this.incomeService.create(income);
 
       return response
         .status(201)
-        .json({ message: "income created", income: result })
+        .json({ message: "income created", income: result });
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ message: "Internal Server Error: ", error });
+    }
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const incomeId: string = request.body.id
+
+    if (!incomeId)
+      return response
+        .status(400)
+        .json({ message: "Bad request: id required!" })
+
+    try {
+      const found: IIncome | null = await this.incomeService.getById(incomeId)
+
+      if (!found)
+        return response
+          .status(404)
+          .json({ message: "Not found: income id!"})
+
+      await this.incomeService.delete(found)
+
+      return response
+        .status(200)
+        .json({ message: "income deleted successfully" })
 
     } catch (error) {
       return response
         .status(500)
-        .json({ message: "Internal Server Error: ", error })
+        .json({ message: "Internal Server Error", error });
     }
   }
 }
