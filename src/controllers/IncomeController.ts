98a4e1b01d5a3bcc0fb6cd885f7aaf8223419b18
vinicius-hso <viniciushso@icloud.com>
2024-04-28 10:53:48 -0300

    feat: add new income controller

--- src/controllers/IncomeController.ts
+++ src/controllers/IncomeController.ts
@@ -1,25 +1,43 @@
 import { Request, Response } from "express";
-// import { UserService, AuthorizationService } from "../services";
-// import { IUser } from "../ts";
-
+import { IncomeService } from "../services/IncomeService";
+import { IIncome } from "../ts/IIncome";
 
 export class IncomeController {
-//   userService: UserService;
-//   authService: AuthorizationService;
+  private incomeService: IncomeService;
 
-  constructor(
-    // userService = new UserService(),
-    // authService = new AuthorizationService()
-  ) {
-    // this.userService = userService;
-    // this.authService = authService;
+  constructor(incomeService = new IncomeService()) {
+    this.incomeService = incomeService;
   }
 
   public async new(request: Request, response: Response): Promise<Response> {
-   const income = request.body
-    
-    return response.status(200).json({ message: "income created", income: income });
-  }
+    const income: IIncome | any = request.body;
 
-  
+    if (!income.title)
+      return response
+        .status(400)
+        .json({ message: "Bad request: title required!" })
+
+    if (!income.total)
+      return response
+        .status(400)
+        .json({ message: "Bad request: total required!" })
+        
+    if (!income.date)
+      return response
+        .status(400)
+        .json({ message: "Bad request: date required!" })
+
+    try {
+      const result = await this.incomeService.create(income)
+
+      return response
+        .status(201)
+        .json({ message: "income created", income: result })
+
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ message: "Internal Server Error: ", error })
+    }
+  }
 }
